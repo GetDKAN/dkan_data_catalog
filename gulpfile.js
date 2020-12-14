8640345096d85aa22fa2b518b064ad5130a059ab
dgading <dgading@gmail.com>
2020-12-14 17:04:23 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(sass({
+      includePaths: ['node_modules']
+    }));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
